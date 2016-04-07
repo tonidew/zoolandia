4a22e08c6470814a587118d7e57d687bee1b5ecc
Toni Dew <Dewsy@Tonis-MBP.hsd1.tn.comcast.net>
2016-04-06 23:32:45 -0500

    updated Animal.cs with genus/species

--- Animal.cs
+++ Animal.cs
@@ -1,11 +1,11 @@
 
-
 // Base class
 class Animal {
     // Simple properties
     private double speed = 0;
-    public string species { get; set; }
     public int legs { get; set; }
+    public bool fur { get; set; }
+    public string sound { get; set;}
 
 
     // Public method
@@ -13,67 +13,110 @@ class Animal {
         Console.WriteLine("Base class walk method");
         speed = speed + (0.1 * legs);
     }
-}
-
-// Derived class
-class Lizard : Animal {
-    // Adding additional properties to what is inherited from Animal
-    public string scaleColor { get; set; }
-    public bool camouflage { get; set; }
-}
-
-
-// Base class
-class Animal {
-    // Simple properties
-    private double speed = 0;
-    public string species { get; set; }
-    public int legs { get; set; }
-    // Your Animal class should include a Public string "AnimalName" that acts as a getter and a setter.
-    public string AnimalName {get; set;}
-
-
-    // Public method
-    public void walk () {
-        Console.WriteLine("Base class walk method");
-        speed = speed + (0.1 * legs);
+    public virtual void eat (string food) {
+        _food = food;
     }
 }
 
-// Create a "Species" Class that inherits from the "Animal" Class. The Species class should include a public string "SpeciesName" that acts as a getter and a setter.
-class Species : Animal {
-    public string SpeciesName {get; set;}
+// FIRST GENUS  - Derived class - (bears)
+class Ursus : Animal {
+    public string Color { get; set; }
 
+    public override void eat (string fish) {
+        _fish = fish;
+    }
+} 
 
-// Create three or more different classes for individual animals you want to include in your zoo. Each individual animal class should inherit from "species" and should include a public string with getter and setter for "Noise". You should also include 3-4 more getter/setters of your own choosing. Try and use at least one int (integer) type and one bool (boolean).
-class Tiger : Species {
-    public string Noise {get;set;}
+// Derived class of Alaskan Brown Bear - the first species
+class Arctos : Ursus {
+    public string Name { get; set; }
+}
+
+// Derived class of Asian Black Bear - the second species
+class Thibetanus : Ursus {
+    public string Name { get; set; }
 
 }
 
-class Lion : Species {
-    public string Noise {get; set;}
-    }
+// Derived class of Newfoundland Black Bear - the third species
+class Americanus hamiltoni : Ursus {
+    public string Name { get; set; }
 
-class Monkey : Species {
-    public string Noise {get;set;}
-    }
+} 
 
+// Derived class of American Black Bear - the fourth species
+class Americanus : Ursus {
+    public string Name { get; set; }
 
-// Derived class
-class Lizard : Animal {
-    // Adding additional properties to what is inherited from Animal
-    public string scaleColor { get; set; }
-    public bool camouflage { get; set; }
 }
 
-Lizard larry = new Lizard();
-larry.legs = 4;
-larry.scaleColor = "Brown";
-larry.camouflage = false;
-larry.walk();
-Console.WriteLine("A {0} lizard moving at {1} m/s", larry.scaleColor, larry.speed);
+// Derived class of Polar Bear - the fifth species
+class Maritimus : Ursus {
+    public string Name { get; set; }
+}
 
+// SECOND GENUS - Derived class - (frogs)
+class Rana : Animal {
+    public string Color { get; set; }
+}
 
+// Derived class of  Northern Red-Legged Frog - first species of frog
+class Aurora : Rana {
+    public string Name { get; set; }
+}
 
+// Derived class of Foothill Yellow Legged Frog - second species of frog
+class Boylii : Rana {
+    public string Name {get; set;} 
+}
 
+// Derived class of Cascades Frog - third species of frog
+class Cascadae : Rana {
+    public string Name { get; set; }
+}
+
+// Derived class of Columbia Spotted Frog - fourth species of frog
+class Luteiventris : Rana {
+    public string Name { get; set; }
+}
+
+// Derived class of Common Frog - fifth species of frog
+class Temporaria : Rana {
+    public string Name { get; set; }
+}
+
+// THIRD GENUS - Derived class - (eagles)
+class Haliaeetus : Animal {
+    public string wingSpan { get; set; }
+
+}
+
+// Derived class of American Bald Eagle - first species of eagle
+class Leucocephalus : Haliaeetus {
+    public string Name { get; set; }
+
+}
+
+// Derived class of Sanford's Sea Eagle - second species of eagle
+class Sanfordi : Haliaeetus {
+    public string Name { get; set; }
+
+}
+
+// Derived class of African Fish Eagle - third species of eagle
+class Vocifer : Haliaeetus {
+    public string Name { get; set; }
+
+}
+
+// Derived class of Steller's Sea Eagle  - fourth species of eagle
+class Pelagicus : Haliaeetus {
+    public string Name { get; set; }
+
+}
+
+// Derived class of White-tailed Eagle - fifth species of eagle
+class Albicilla : Haliaeetus {
+    public string Name { get; set; }
+
+}
